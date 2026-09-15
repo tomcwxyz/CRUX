610ebfd7bd82b346d861f6c3b6ca32bd16ac3f3c
Tom C Watson <150357824+tomcwxyz@users.noreply.github.com>
2026-09-15 12:06:10 +0100

    Export evaluation case schema

--- packages/schemas/src/index.ts
+++ packages/schemas/src/index.ts
@@ -2,5 +2,6 @@ export * from "./primitives.js";
 export * from "./claim.js";
 export * from "./evidence.js";
 export * from "./evaluation.js";
+export * from "./evaluation-case.js";
 export * from "./system.js";
 export * from "./trace.js";
