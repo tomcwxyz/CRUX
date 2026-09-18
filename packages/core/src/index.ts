199ef31ccbc728d1a2fccfa47ce2162298a64677
Tom C Watson <150357824+tomcwxyz@users.noreply.github.com>
2026-09-18 20:46:17 +0100

    Export gateway discovery adapter

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -14,6 +14,7 @@ import {
 export * from "./scope.js";
 export * from "./discovery.js";
 export * from "./connectors.js";
+export * from "./gatewayDiscovery.js";
 
 export type DerivedClaimStatus =
   | "declared"
